d8da10abff80abae6bd16051b7b82546084e59ab
Aina <ainaeinikyte@gmail.com>
2024-03-06 22:08:29 +0200

    Bootstrap

--- pages/Page404.js
+++ pages/Page404.js
@@ -1,10 +1,11 @@
 import { PageTemplate } from "../lib/PageTemplate.js";
 
 class Page404 extends PageTemplate {
-    constructor() {
-        super();
-        this.pageTitle = '404';
-    }
+  constructor() {
+    super();
+    this.pageTitle = "404";
+  }
+
   main() {
     return `<main>404 PAGE CONTENT</main>`;
   }
