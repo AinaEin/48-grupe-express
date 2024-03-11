60bd9b5d4a1ab26f0f0a10efd15ae5bfcc825b31
Aina <ainaeinikyte@gmail.com>
2024-03-11 21:41:43 +0200

    Routing projects

--- data/servicesData.js
+++ data/servicesData.js
@@ -1,30 +1,36 @@
 const servicesData = [
   {
-    id: "design",
-    title: "Design",
-    titleTeam: "Design Team",
-    href: "/services/design",
-    hrefTeam: "/services/design/design-team",
+    id: "devops",
+    title: "DevOps",
+    titleTeam: "DevOps Team",
+    href: "/services/devops",
+    hrefTeam: "/services/devops/devops-team",
     promoText:
-      "Elevate your brand with cutting-edge design solutions that blend creativity and functionality seamlessly. Our team of experienced designers is dedicated to crafting visually stunning and user-centric designs that captivate your audience and leave a lasting impression.",
+      "Unleash the power of seamless collaboration and accelerated development with our DevOps solutions. Streamline your software delivery pipeline, enhance efficiency, and minimize downtime by integrating development and operations seamlessly. Elevate your business agility with our expert DevOps team, ensuring continuous innovation and rapid deployment, so you can focus on what truly matters – delivering exceptional products and services to your customers.",
+    projectText:
+      "Embark on a transformative journey with our DevOps project solutions. From automating repetitive tasks to orchestrating complex workflows, our DevOps experts ensure a smooth and efficient collaboration between development and operations teams. Experience accelerated delivery cycles, reduced time-to-market, and unparalleled scalability, as we optimize your processes for maximum agility and innovation.",
   },
   {
     id: "web-development",
     title: "Web development",
     titleTeam: "Web development Team",
     href: "/services/web-development",
-    hrefTeam: "/services/:serviceId/web-development-team",
+    hrefTeam: "/services/web-development/web-development-team",
     promoText:
       "Transform your online presence with our web development prowess. Whether you need a sleek and responsive website, a robust e-commerce platform, or a custom web application, our skilled developers are committed to bringing your digital vision to life, delivering high-performance solutions that drive results.",
+    projectText:
+      "Craft immersive digital experiences that resonate with your audience through our UX project expertise. Our seasoned designers delve into user research, wireframing, and prototyping to create intuitive and visually stunning interfaces. From empathetic design thinking to meticulous user testing, our UX projects guarantee an engaging and memorable journey for your users, fostering brand loyalty and satisfaction.",
   },
   {
     id: "ux",
     title: "UX",
     titleTeam: "UX Team",
     href: "/services/ux",
-    hrefTeam: "/services/:serviceId/ux-team",
+    hrefTeam: "/services/ux/ux-team",
     promoText:
       "Revolutionize user experiences with our UX expertise that goes beyond aesthetics. From intuitive navigation to engaging interactions, we specialize in creating digital experiences that not only meet but exceed user expectations, ensuring a seamless and enjoyable journey for your audience.",
+    projectText:
+      "Revolutionize your online presence with our web development prowess. Our team specializes in creating bespoke websites and applications tailored to your unique needs. From responsive design for optimal user experience to robust backend development for seamless functionality, our web development projects ensure your digital footprint is not just an online presence but a powerful tool driving your business forward.",
   },
 ];
 
--- pages/PageServiceTeam.js
+++ pages/PageServiceTeam.js
@@ -1,20 +1,33 @@
 import { PageTemplate } from "../lib/PageTemplate.js";
+import { servicesData } from "../data/servicesData.js";
 
 class PagesServiceTeam extends PageTemplate {
-  constructor() {
+  serviceObj;
+
+  constructor(params) {
     super();
-    this.pageTitle = 'Design Team';
+    this.params = params;
+    this.getServiceTeam();
   }
 
   main() {
     return `
             <main>
                 <section class="container py-5">
-                    <h1 class="display-5 fw-bold">${this.pageTitle} page</h1>
-                    <p class="col-md-8 fs-4">Using a series of utilities, you can create this jumbotron, just like the one in previous versions of Bootstrap. Check out the examples below for how you can remix and restyle it to your liking.</p>
-                    <a href="${"#"}" class="btn btn-primary btn-lg">Design team</a>
+                    <h1 class="display-5 fw-bold">${this.serviceObj.titleTeam}</h1>
+                    <p class="col-md-8 fs-4">${this.serviceObj.projectText}</p>
+                    <a href="${"#"}" class="btn btn-primary btn-lg">Projects</a>
                 </section>
             </main>`;
+  }
+
+  getServiceTeam() {
+    for (const service of servicesData) {
+      if (service.id === this.params.serviceId) {
+        this.serviceObj = service;
+        break;
+      }
+    }
   }
 }
 
