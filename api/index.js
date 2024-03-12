020238447000f885fa2740fbc8a13bf4ab3830da
Aina <ainaeinikyte@gmail.com>
2024-03-12 22:25:44 +0200

    feat: add sum api route

--- api/index.js
+++ api/index.js
@@ -1,9 +1,16 @@
 import { Router } from "express";
 
-const apiRouter = Router ();
+const apiRouter = Router();
 
-apiRouter.get('/api', (req, res) => {
-    res.send('Sveiki atvyke i API puslapi...')
+apiRouter.get("/api", (req, res) => {
+  res.send("Sveiki atvyke i API puslapi...");
+});
+
+apiRouter.get("/api/sum/:num1/:num2", (req, res) => {
+  const num1 = +req.params.num1;
+  const num2 = +req.params.num2;
+  const sum = num1 + num2;
+  res.send(`${num1} + ${num2} = ${sum}`);
 });
 
 export { apiRouter };
