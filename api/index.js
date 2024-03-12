c187288799b5f1fde84758e06bfa6045dd880ca8
Aina <ainaeinikyte@gmail.com>
2024-03-12 23:10:39 +0200

    feat: api fix

--- api/index.js
+++ api/index.js
@@ -9,8 +9,29 @@ apiRouter.get("/api", (req, res) => {
 apiRouter.get("/api/sum/:num1/:num2", (req, res) => {
   const num1 = +req.params.num1;
   const num2 = +req.params.num2;
-  const sum = num1 + num2;
-  res.send(`${num1} + ${num2} = ${sum}`);
+  const answer = num1 + num2;
+  res.send(`${num1} + ${num2} = ${answer}`);
+});
+
+apiRouter.get("/api/minus/:num1/:num2", (req, res) => {
+  const num1 = +req.params.num1;
+  const num2 = +req.params.num2;
+  const answer = num1 - num2;
+  res.send(`${num1} - ${num2} = ${answer}`);
+});
+
+apiRouter.get("/api/multiplication/:num1/:num2", (req, res) => {
+  const num1 = +req.params.num1;
+  const num2 = +req.params.num2;
+  const answer = num1 * num2;
+  res.send(`${num1} * ${num2} = ${answer}`);
+});
+
+apiRouter.get("/api/division/:num1/:num2", (req, res) => {
+  const num1 = +req.params.num1;
+  const num2 = +req.params.num2;
+  const answer = num1 / num2;
+  res.send(`${num1} / ${num2} = ${answer}`);
 });
 
 export { apiRouter };
