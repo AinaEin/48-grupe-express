b89aa58f41e5fde1c85aa19550028f7713824a9b
Aina <ainaeinikyte@gmail.com>
2024-03-13 19:16:19 +0200

    feat: added apiRouter time

--- api/index.js
+++ api/index.js
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { timeToWords }  from "../data/numbertoword.js";
 
 const apiRouter = Router();
 
@@ -32,6 +33,31 @@ apiRouter.get("/api/division/:num1/:num2", (req, res) => {
   const num2 = +req.params.num2;
   const answer = num1 / num2;
   res.send(`${num1} / ${num2} = ${answer}`);
+});
+
+apiRouter.get("/api/abbr/:name/:lastName", (req, res) => {
+  const name = req.params.name;
+  const lastName = req.params.lastName;
+  const answer = name.charAt(0) + '. ' + lastName.charAt(0) + '.';
+  res.send(`${name.charAt(0)} + ${lastName.charAt(0)} = ${answer}`);
+});
+
+
+apiRouter.get("/api/time/:hh/:mm/:ss", (req, res) => {
+  const hh = +req.params.hh;
+  const mm = +req.params.mm;
+  const ss = +req.params.ss;
+  const answer = hh + ":" + mm + ":" + ss;
+  res.send(
+    `${hh} + ${mm} + ${ss}= ${answer}`
+  );
+});
+
+apiRouter.get("/api/time-to-text/:hh/:mm/:ss", (req, res) => {
+  const hours = +req.params.hh;
+  const minutes = +req.params.mm;
+  const seconds = +req.params.ss;
+  res.send(timeToWords(hours, minutes, seconds));
 });
 
 export { apiRouter };
